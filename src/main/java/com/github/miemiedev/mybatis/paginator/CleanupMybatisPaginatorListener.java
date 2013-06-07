6364eb6f24a1284b0967afc720674fc67a6bdde4
miemiedev <miemiedev@gmail.com>
2013-06-07 21:20:10 +0800

    Rename

---
+++ src/main/java/com/github/miemiedev/mybatis/paginator/CleanupMybatisPaginatorListener.java
@@ -0,0 +1,17 @@
+package com.github.miemiedev.mybatis.paginator;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ */
+public class CleanupMybatisPaginatorListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        OffsetLimitInterceptor.Pool.shutdownNow();
+    }
+}
